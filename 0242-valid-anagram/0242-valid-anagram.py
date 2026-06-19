58c17dc0648207761646c42f44d17d802344cde3
syam prakash maddineni <msprakash2907@gmail.com>
2026-06-19 10:13:00 -0500

    Time: 903 ms (5.55%), Space: 19.3 MB (75.45%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,17 +1,9 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        
-        def makeaset(s: str):
-            d={}
-            for i in s:
-                if i not in d:
-                    d[i]=1
-                else:
-                    d[i]+=1
-            return d
-        ss=makeaset(s)
-        st=makeaset(t)
-        if ss==st:
-            return True
-        else:
+        if len(s)!=len(t):
             return False
+        for i in s:
+            if s.count(i)!=t.count(i):
+                return False
+        return True
+        
